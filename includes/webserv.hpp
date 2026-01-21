fd09cbfb8eef2e7ec6e3e2d03b8095d60e49bebf
zuccc23 <daniyaahmane@gmail.com>
2026-01-21 18:15:15 +0100

    makefile + fichiers de base

---
+++ includes/webserv.hpp
@@ -0,0 +1,11 @@
+#ifndef WEBSERV_HPP
+
+# define WEBSERV_HPP
+
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../includes/webserv.hpp"
+
+int main (int ac, char **av)
+{
+	(void)ac;
+	(void)av;
+
+}
