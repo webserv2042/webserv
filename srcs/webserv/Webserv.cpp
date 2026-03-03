2dc67735fc3cc8cc2816b188c8c104fe90b749e9
dna-ahm <daniyaahmane@gmail.com>
2026-03-03 12:06:34 +0100

    pushing again with the webserv files added back

---
+++ srcs/webserv/Webserv.cpp
@@ -0,0 +1,225 @@
+#include "../../includes/Webserv.hpp"
+#include "../../includes/server/Client.hpp"
+#include "../../includes/Signals.hpp"
+
+//CONSTRUCTOR(S)
+Webserv::Webserv() {
+    ep_fd = -1;
+	ready_fds = -1;
+}
+
+//DESTRUCTOR
+Webserv::~Webserv() {
+}
+
+//PUBLIC
+
+/// @brief boucle principale epoll : orchestre l'attente et la reception de connections
+void    Webserv::epollLoop()
+{
+	startEpoll();
+
+	while (server_running)
+	{
+		//epoll_wait attend de recevoir des connections
+		std::cout << "\033[90mWAITING...\033[0m" << std::endl;
+        if (waitForEvents() == SIGNAL_RECEIVED)
+            break;
+		
+		checkIdleTimeout();
+
+		//connection(s) trouvee(s) -> parcourir les events
+		for (int i = 0; i < ready_fds; i++)
+		{
+			int fd = events[i].data.fd;
+
+			if (isSocketFd(fd) == true) //nouvelle connexion sur une socket -> accepter
+				acceptClient(fd);
+
+			else //requete/reponse client -> lire/ecrire
+			{
+				//deconnexion client
+				if (events[i].events & (EPOLLHUP | EPOLLERR))
+				{
+					std::cout << "\033[31mDISCONNECT !!!!\033[0m" << std::endl;
+					epoll_ctl(ep_fd, EPOLL_CTL_DEL, fd, NULL);
+					clients[fd].~Client();
+					close(fd);
+				}
+
+				//lire la requete, la parser, preparer la reponse etc...
+				else if (events[i].events == EPOLLIN)
+					treatRequest(fd);
+
+				//renvoyer la reponse
+				else if (events[i].events == EPOLLOUT)
+					sendResponse(clients[fd]);
+			}
+			std::cout << "--------------------------" << std::endl;
+		}
+	}
+
+	std::cout << "Server stopped." << std::endl;
+    finalClean();
+}
+
+/// @brief ajoute le vecteur de serveurs a webserv
+/// @param servVec les serveurs a ajouter
+void    Webserv::setServers(std::vector<Server> &servVec)
+{
+	servers = servVec;
+	for (size_t i = 0; i < servers.size(); i++)
+		servers[i].init();
+}
+
+//PRIVATE
+
+/// @brief lis et traite la requete d'un client
+/// @param fd fd du client
+void	Webserv::treatRequest(int &fd)
+{
+	std::cout << "\033[34mREADING REQUEST...\033[0m" << std::endl;
+	int readReturn = testRead(fd);
+	if (readReturn == DISCONNECTION)
+		return;
+
+	clients[fd].lastActivity = time(NULL);
+
+	if (clients[fd].clientState == PROCESSING)
+		modifyEpollout(fd, ADD_EPOLLOUT);
+}
+
+/// @brief fonction pour tester de lire a partir du fd client (a supp + tard)
+/// @param clientFD fd du client
+int	Webserv::testRead(int &clientFD)
+{
+	clients[clientFD].clientState = READING_REQUEST;
+	char buffer[2000];
+
+	int bytesRead = read(clientFD, buffer, sizeof(buffer));
+	if (bytesRead == ERROR || bytesRead == 0) //deconnexion client
+	{
+		closeClient(clientFD);
+		return DISCONNECTION;
+	}
+
+	std::cout << std::endl << "Content:" << std::endl;
+	buffer[bytesRead] = '\0';
+	std::cout << buffer << std::endl;
+	memset(buffer, 0, 2000);
+
+	//rempli la reponse temporairement
+	// clients[clientFD].writeBuff = "test response: hi!\n";
+	// clients[clientFD].buffSize = clients[clientFD].writeBuff.size();
+
+
+	//test avec bcp de data
+	// std::string bigBody(5 * 1024 * 1024, 'A'); // 5 MB of 'A'
+	// std::string str;
+	// std::stringstream ss;
+	// ss << bigBody.size();
+	// ss >> str;
+	// clients[clientFD].writeBuff =
+    // "HTTP/1.1 200 OK\r\n"
+    // "Content-Length: " + str + "\r\n"
+    // "Content-Type: text/plain\r\n"
+    // "Connection: close\r\n"
+    // "\r\n" +
+    // bigBody + "\n--END--\n";
+	// clients[clientFD].buffSize = clients[clientFD].writeBuff.size();
+
+	//test avec une vraie reponse http
+	std::string body = readFile("./www/index.html");
+
+	std::string str;
+	std::stringstream ss;
+	ss << body.size();
+	ss >> str;
+
+	clients[clientFD].writeBuff =
+	"HTTP/1.1 200 OK\r\n"
+    "Date: Wed, 25 Feb 2026 16:14:00 GMT\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n"
+    "Content-Length: " + str + "\r\n"
+    "Connection: close\r\n"
+    "\r\n" +
+	body;
+
+	clients[clientFD].buffSize = clients[clientFD].writeBuff.size();
+	
+	clients[clientFD].clientState = PROCESSING;
+	return 0;
+}
+
+/// @brief fonction qui renvoi une reponse au client en non-bloquant(=potentiellement en plusieurs fois)
+/// @param client le client a qui on veut renvoyer la reponse
+void	Webserv::sendResponse(Client &client)
+{
+	std::cout << "\033[34mSENDING RESPONSE...\033[0m" << std::endl;
+	client.clientState = WRITING_RESPONSE;
+
+	//Calcule le nombre de bytes restants a envoyer
+	int currentBytes = 0;
+	int bytesLeft = client.buffSize - client.bytesSent;
+
+	//s'il y en a encore, les envoyer
+	if (bytesLeft > 0)
+	{
+		currentBytes = send(client.clientFd, &client.writeBuff[client.bytesSent], bytesLeft, MSG_DONTWAIT);
+		if (currentBytes <= 0)
+		{
+			if (errno == EAGAIN || errno == EWOULDBLOCK) //mode non bloquant active, le renvoi sera reessaye
+				return;
+			else //erreur d'envoi -> client supprime
+			{
+				std::cerr << "(SERVER) couldn't send response" << std::endl;
+				closeClient(client.clientFd);
+				return;
+			}
+		}
+		else //ajout des bytes envoyés au compte
+			client.bytesSent += currentBytes;
+	}
+
+	if (client.bytesSent == client.buffSize) // tous les bytes envoyes? ->client retourne en EPOLLIN, sinon retourne ds la boucle d'envoi
+	{
+		client.resetClient();
+		modifyEpollout(client.clientFd, DELETE_EPOLLOUT);
+	}
+	client.lastActivity = time(NULL);
+}
+
+/// @brief ferme et supprime les donnees necessaires a la fin de la boucle ->
+/// clients, server sockets, epoll instance
+void    Webserv::finalClean()
+{
+    //close clients fd + whole client list
+    std::map<int, Client>::iterator it;
+
+    for (it = clients.begin(); it != clients.end(); it++)
+    {
+        if (it->first != -1)
+            close(it->first);
+    }
+    clients.clear();
+
+    //close server socketFD
+    for (long unsigned int i = 0; i < servers.size(); i++)
+    {
+        if (servers[i].getSocketFD() != -1)
+            close(servers[i].getSocketFD());
+        servers[i].setSocketFD(-1);
+    }
+
+    //close epoll instance
+    if (ep_fd != -1)
+        close(ep_fd);
+    ep_fd = -1;
+}
+
+//gerer le timeout OK
+//gerer les gros buffers OK
+//gerer pollout OK
+//gerer les signaux OK
+//gerer les leaks/fd OK
+//gerer plusieurs servers en mm temps OK
---
+++ srcs/webserv/epollUtils.cpp
@@ -0,0 +1,81 @@
+#include "../../includes/Webserv.hpp"
+#include "../../includes/server/Client.hpp"
+#include "../../includes/Signals.hpp"
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+/// @brief rend un fd de socket non-bloquant
+/// @param fdSocket le fd a rendre non-bloquant
+void    Webserv::setNonBlockingSocket(int &fdSocket)
+{
+	int flags = fcntl(fdSocket, F_GETFL);
+	if (flags == ERROR)
+		throw std::runtime_error("(SERVER) Failed to get socket configuration: ");
+	flags = flags | O_NONBLOCK;
+
+	fcntl(fdSocket, F_SETFL, flags);
+}
+
+/// @brief ajoute un nouveau fd a la liste de surveillance selon l'evenement precise
+/// @param newFd le fd a surveiller
+/// @param epEvent l'evenement a surveiller (ex: EPOLLIN, EPOLLOUT...)
+void    Webserv::registerNewFd(const int &newFd, uint32_t epEvent)
+{
+	//definir l'evenement que l'on veut surveiller(nouvelles connections) + sur quel fd
+	struct epoll_event event;
+	event.events = epEvent;
+	event.data.fd = newFd;
+
+	//ajouter le fd de notre socket a la liste de surveillance
+	if (epoll_ctl(ep_fd, EPOLL_CTL_ADD, newFd, &event) == ERROR)
+		throw std::runtime_error("(SERVER) epoll_ctl function failed: ");
+}
+
+/// @brief check si le fd est un socket ou un client
+/// @param sockFD fd a verifier
+/// @return true/false
+bool    Webserv::isSocketFd(int &sockFD)
+{
+    for (long unsigned int i = 0; i < servers.size(); i++)
+    {
+        if (sockFD == servers[i].getSocketFD())
+            return true;
+    }
+    return false;
+}
+
+/// @brief change l'event surveille d'un fd pour y ajouter/enlever EPOLLOUT selon l'action
+/// @param fd le fd a modifier
+/// @param action ADD_EPOLLOUT(0) = ajouter epollout DELETE_EPOLLOUT(1) enlever epollout
+void    Webserv::modifyEpollout(int &fd, int action)
+{
+	//definir l'evenement que l'on veut surveiller(EPOLLOUT pour pouvoir ecrire, EPOLLIN pour rester en keep-alive)
+    // + sur quel fd
+	struct epoll_event event;
+    if (action == ADD_EPOLLOUT)
+	    event.events = EPOLLIN | EPOLLOUT;
+    if (action == DELETE_EPOLLOUT)
+        event.events = EPOLLIN;
+    event.data.fd = fd;
+    
+	//modifier la surveillance du fd avec epoll_ctl
+	if (epoll_ctl(ep_fd, EPOLL_CTL_MOD, fd, &event) == ERROR)
+		throw std::runtime_error("(SERVER) epoll_ctl function failed: ");
+}
+
+std::string readFile(const std::string& filename)
+{
+    std::ifstream file(filename.c_str());
+    if (!file.is_open())
+	{
+		std::cout << "ERROR FILENAME" << std::endl;
+		return ""; // or handle error differently
+	}
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+
+    return buffer.str();
+}
